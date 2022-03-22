ffcdc07471764a4fd50411cb1652c3db3ea4e8d8
Rajni <102152739+rajni-B@users.noreply.github.com>
2022-03-22 16:42:17 +0530

    Add files via upload

---
+++ home/form.py
@@ -0,0 +1,9 @@
+from attr import field
+from django import forms
+from froala_editor.widgets import FroalaEditor
+from .models import*
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = BlogModel
+        fields = ['content']
